414943722461d4d4336dbdd92d42d82eeaf8d2cf
fanpero87 <FAb1hn0!>
2021-02-07 14:51:17 -0500

    static files

--- project3/settings.py
+++ project3/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,4 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# This determines the location where static files are being pulled from.
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
